6bf360de93443d238c2fff51ae711c71b529b6c4
ivan <kulazhenkov.ivan@yandex.ru>
2026-09-23 15:46:08 +0300

    Создание основные классов для работы

---
+++ src/Game.java
@@ -0,0 +1,12 @@
+public class Game {
+
+    int attempts;
+
+    static void printGameMenu() {
+
+        System.out.println("Меню:");
+        System.out.println("1. Начать игру:");
+        System.out.println("2. Вывести список лучших результатов");
+        System.out.println("3. Покинуть игру");
+    }
+}
---
+++ src/Menu.java
@@ -0,0 +1,38 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Menu {
+
+    static void printMenu(Scanner scanner) {
+        int input;
+        boolean running = true;
+
+        while (running) {
+            System.out.println("Меню:");
+            System.out.println("1. Начать игру:");
+            System.out.println("2. Вывести список лучших результатов");
+            System.out.println("3. Покинуть игру");
+            System.out.println("Твой выбор: ");
+
+            try {
+                input = scanner.nextInt();
+
+            } catch (InputMismatchException e) {
+                System.out.println("Нужно вводить число!");
+                scanner.next();
+                continue;
+            }
+
+            switch(input) {
+                case 1: {
+                    Game.printGameMenu();
+                }
+
+                case 2: {
+
+                }
+            }
+        }
+
+    }
+}
