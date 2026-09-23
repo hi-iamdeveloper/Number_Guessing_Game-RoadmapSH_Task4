a544525bc4d08dc2dbec29e56821d033159bce88
ivan <kulazhenkov.ivan@yandex.ru>
2026-09-23 15:15:45 +0300

    Инициализация проекта

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+void main() {
+
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Добро пожаловать в игру угадай цифру!");
+    System.out.println("Я загадаю цифру от 1 до 100, а твоя задача - ее угадать");
+
+
+
+
+}
+
